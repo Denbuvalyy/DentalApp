29bd199c5e954bbca7e4b7ade0e7ef61d6494896
Denbuvalyy <abcabcabcdenis@gmail.com>
2026-05-02 20:46:57 +0300

    tests added

---
+++ DentalApp.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+﻿namespace DentalApp.Tests;
+
+public class SampleTests
+{
+    [Fact]
+    public void Should_Add_Numbers()
+    {
+        var result = 2 + 2;
+        Assert.Equal(5, result);
+        //Assert.Equal(4, result);
+    }
+}
